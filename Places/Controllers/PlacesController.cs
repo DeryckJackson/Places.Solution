e689922f392f448160b1ae6a3f376717cca2c2f0
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-07-22 09:37:23 -0700

    initial commit
    Co-authored-by: Peter Grimm <pagrimm@gmail.com>

---
+++ Places/Controllers/PlacesController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+using System.Collections.Generic;
+
+namespace Places.Controllers
+{
+  public class PlacesController : Controller
+  {
+
+    [HttpGet("/items")]
+    public ActionResult Index()
+    {
+      List<Item> allItems = Item.GetAll();
+      return View(allItems);
+    }
+
+    [HttpGet("/items/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/items")]
+    public ActionResult Create(string description)
+    {
+      Item myItem = new Item(description);
+      return RedirectToAction("Index");
+    }
+
+    [HttpPost("/items/delete")]
+    public ActionResult DeleteAll()
+    {
+      Item.ClearAll();
+      return View();
+    }
+
+  }
+}
